34405a6452dc2ce6a12390adde4f772f0f1f1e8c
Nikolay Tetus <nikolaytetus@gmail.com>
2021-04-11 17:58:14 +0300

    Added *this in server class Deleted useless comments Changed const to address structure, in class member *(line: 13, server.cpp)*

--- server.cpp
+++ server.cpp
@@ -2,25 +2,22 @@
 
 server::server(unsigned short int port)
 {
-    address.sin_family      = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port        = htons(port);
+    this->address.sin_family      = AF_INET;
+    this->address.sin_addr.s_addr = INADDR_ANY;
+    this->address.sin_port        = htons(port);
 }
 
 int server::startListen()
 {
-    //exit(EXIT_FAILURE);
-    //perror("listen");
-
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    if ((this->server_fd = socket(this->address.sin_family, SOCK_STREAM, 0)) == 0)
     {
         //Socket failed
         return -1;
     }
 
     // Forcefully attaching socket to the port
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+    if (setsockopt(this->server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                   &opt, sizeof(opt)))
     {
         //setsockopt
@@ -28,14 +25,14 @@ int server::startListen()
     }
 
     // Forcefully attaching socket to the port
-    if (bind(server_fd, reinterpret_cast<struct sockaddr *>(&address), sizeof(address)) < 0)
+    if (bind(this->server_fd, reinterpret_cast<struct sockaddr *>(&address), sizeof(address)) < 0)
     {
         //Bind failed
         return -3;
     }
 
     //Starting listening
-    if (listen(server_fd, 3) < 0)
+    if (listen(this->server_fd, 3) < 0)
     {
         //Listen
         return -4;
@@ -48,7 +45,7 @@ int server::accept()
 {
     int addrlen = sizeof(address);
 
-    if ((new_socket = ::accept(server_fd, reinterpret_cast<struct sockaddr *>(&address), reinterpret_cast<socklen_t*>(&addrlen))) < 0)
+    if ((this->new_socket = ::accept(server_fd, reinterpret_cast<struct sockaddr *>(&address), reinterpret_cast<socklen_t*>(&addrlen))) < 0)
     {
         return -1;
     }
@@ -59,7 +56,7 @@ int server::accept()
 int server::send(string message)
 {
     //Call from global namespace in sys/socket.h
-    ::send(new_socket, message.c_str(), message.length(), 0);
+    ::send(this->new_socket, message.c_str(), message.length(), 0);
     return 0;
 }
 
@@ -67,7 +64,7 @@ string server::read()
 {
     char buffer[4096];
     string message = "";
-    valread = ::read(new_socket, buffer, 4096);
+    valread = ::read(this->new_socket, buffer, 4096);
     if (valread)
         message = string(buffer, static_cast<unsigned long>(valread));
 
